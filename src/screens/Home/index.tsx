3968906b4fc4c0b2aa561cdad11d0ac376a832b2
mario.coxe <mario.coxe@ideiasdinamicas.com>
2024-08-05 19:22:15 +0100

    refactory: improving the home component and making it more beautiful

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,26 @@
-import { Text, View } from "react-native";
+import { Text, View, TextInput, TouchableOpacity } from "react-native";
 import { styles } from "./styles";
 
-export default function Home() {
+export function Home() {
+  function hendleParticipantAdd() {
+    console.log("Cliclout");
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do Evento</Text>
       <Text style={styles.eventData}>Sexta, 4 de Setembro de 2022</Text>
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor={"#6B6B6B"}
+        />
+        <TouchableOpacity style={styles.button} onPress={hendleParticipantAdd}>
+          <Text style={styles.buttonText}> + </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
