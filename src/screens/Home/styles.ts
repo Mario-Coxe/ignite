2515dc956ccf64cbe9988f775fc6a197b6e0ffa2
mario.coxe <mario.coxe@ideiasdinamicas.com>
2024-08-05 18:43:43 +0100

    feat: organizing the project

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#131016",
+      padding: 24,
+    },
+    eventName: {
+      color: "#FFF",
+      fontWeight: "bold",
+      fontSize: 24,
+      marginTop: 48,
+    },
+    eventData: {
+      color: "#6B6B6B",
+      fontSize: 16,
+    },
+  });
+  
