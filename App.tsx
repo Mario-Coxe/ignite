18c60f7d526a12450d1ce4ba8e9fbc179344dae0
mario.coxe <mario.coxe@ideiasdinamicas.com>
2024-08-05 22:01:55 +0100

    feat: placing the statusbar overlaying the application

--- App.tsx
+++ App.tsx
@@ -1,5 +1,16 @@
+import React from "react";
+import { StatusBar, View } from "react-native";
 import { Home } from "./src/screens/Home";
 
 export default function App() {
-  return <Home />;
+  return (
+    <View style={{ flex: 1 }}>
+      <StatusBar
+        translucent
+        barStyle={"light-content"}
+        backgroundColor={"transparent"}
+      />
+      <Home />
+    </View>
+  );
 }
